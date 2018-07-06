ff4998e378292c2faf88f579a7cd2bd50101d7fc
Vijay <vijaynats@gmail.com>
2018-07-07 03:18:02 +0530

    renamed MD5Hasher to Hasher

---
+++ Crypto.Basics/MD5Hasher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypto.Basics
+{
+    public class MD5Hasher
+    {
+        public static string getMD5Hash(string str)
+        {
+            MD5CryptoServiceProvider hasher = new MD5CryptoServiceProvider();
+
+            return getHexString(hasher.ComputeHash(Encoding.Default.GetBytes(str)));
+        }
+
+        private static string getHexString(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Crypto.Basics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Security;
+using System.Security.Cryptography;
+
+namespace Crypto.Basics
+{
+    public class Program
+    {
+        // Generate a MD5 hash
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("** Computes MD5 Hash using .NET Crypto Library");
+                Console.Write("Enter a String : ");
+                string str = Console.ReadLine();
+
+                Console.WriteLine("Computing MD5 hash...");
+
+                string shash = MD5Hasher.getMD5Hash(str);
+
+                Console.WriteLine("MD5 Hash is : {0}\nLength of Hash : {1}", shash, shash.Length);
+                Console.Write("Try again ? (y/n) : ");
+            } while (Console.ReadLine() == "y");
+        }
+    }
+}
